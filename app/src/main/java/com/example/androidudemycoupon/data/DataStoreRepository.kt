8a3820c4511cfa257310d03d1b813f51a9b2b233
huythanh0x <huythanh0x@gmail.com>
2023-05-08 22:52:31 +0700

    update setting fragment

--- app/src/main/java/com/example/androidudemycoupon/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/androidudemycoupon/data/DataStoreRepository.kt
@@ -1,9 +1,12 @@
 package com.example.androidudemycoupon.data
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
 import com.example.androidudemycoupon.util.Constants
 import dagger.hilt.android.scopes.ActivityRetainedScoped
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +18,14 @@ import javax.inject.Inject
 class DataStoreRepository @Inject constructor(private val dataStore: DataStore<Preferences>) {
     private object PreferencesKey {
         val fetchedDateTime = stringPreferencesKey(Constants.PREFERENCES_FETCHED_DATE_TIME)
+        val darkMode = booleanPreferencesKey(Constants.PREFERENCES_DARK_MODE)
+        val couponNotification = booleanPreferencesKey(Constants.PREFERENCES_COUPON_NOTIFICATION)
+        val preferCategories = stringSetPreferencesKey(Constants.PREFERENCES_PREFER_CATEGORIES)
+        val preferKeyWords = stringSetPreferencesKey(Constants.PREFERENCES_PREFER_KEYWORDS)
+        val notificationByKeywords =
+            booleanPreferencesKey(Constants.PREFERENCES_NOTIFICATION_BY_KEYWORDS)
+        val notificationByCategories =
+            booleanPreferencesKey(Constants.PREFERENCES_NOTIFICATION_BY_CATEGORIES)
     }
 
     suspend fun saveFetchedDateTime(fetchedDateTime: String) {
@@ -28,6 +39,121 @@ class DataStoreRepository @Inject constructor(private val dataStore: DataStore<P
             throw Exception("ERROR AT getFetchedDateTime()")
         }.map {
             it[PreferencesKey.fetchedDateTime] ?: ""
+        }
+    }
+
+    fun getDarkMode(): Flow<Boolean> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getDarkMode()")
+        }.map {
+            it[PreferencesKey.darkMode] ?: false
+        }
+    }
+
+    suspend fun saveDarkMode(isDarkMode: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.darkMode] = isDarkMode
+        }
+    }
+
+    fun getNotificationByKeyword(): Flow<Boolean> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getDarkMode()")
+        }.map {
+            it[PreferencesKey.notificationByKeywords] ?: true
+        }
+    }
+
+    suspend fun saveNotificationByKeyword(isNotificationByKeywords: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.notificationByKeywords] = isNotificationByKeywords
+        }
+    }
+
+    fun getNotificationByCategories(): Flow<Boolean> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getDarkMode()")
+        }.map {
+            it[PreferencesKey.notificationByCategories] ?: true
+        }
+    }
+
+    suspend fun saveNotificationByCategories(isNotificationByCategories: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.notificationByCategories] = isNotificationByCategories
+        }
+    }
+
+    fun getCouponNotification(): Flow<Boolean> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getDarkMode()")
+        }.map {
+            it[PreferencesKey.couponNotification] ?: true
+        }
+    }
+
+    suspend fun saveCouponNotification(isCouponNotification: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.couponNotification] = isCouponNotification
+        }
+    }
+
+    fun getPreferCategories(): Flow<Set<String>> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getPreferCategories()")
+        }.map { preferences ->
+            preferences[PreferencesKey.preferCategories] ?: setOf(
+                "Development", "IT & Software", "Design"
+            )
+        }
+    }
+
+    suspend fun addPreferCategories(category: String) {
+        dataStore.edit { preferences ->
+            val currentSet =
+                preferences[PreferencesKey.preferCategories]?.toMutableSet() ?: mutableSetOf()
+            currentSet.add(category)
+            Log.d("addPreferCategories", currentSet.toString())
+            preferences[PreferencesKey.preferCategories] = currentSet
+        }
+    }
+
+    suspend fun removePreferCategories(category: String) {
+        dataStore.edit { preferences ->
+            val currentSet =
+                preferences[PreferencesKey.preferCategories]?.toMutableSet() ?: mutableSetOf()
+            currentSet.remove(category)
+            Log.d("addPreferCategories", currentSet.toString())
+            preferences[PreferencesKey.preferCategories] = currentSet
+        }
+    }
+
+    fun getPreferKeywords(): Flow<Set<String>> {
+        return dataStore.data.catch {
+            throw java.lang.Exception("Error at getPreferKeywords()")
+        }.map { preferences ->
+            preferences[PreferencesKey.preferKeyWords] ?: setOf(
+                "python", "kotlin", "android"
+            )
+        }
+    }
+
+    suspend fun addPreferKeywords(keyword: String) {
+        dataStore.edit { preferences ->
+            val currentSet =
+                preferences[PreferencesKey.preferKeyWords]?.toMutableSet() ?: mutableSetOf()
+            currentSet.add(keyword)
+            preferences[PreferencesKey.preferKeyWords] = currentSet
+        }
+    }
+
+    suspend fun removePreferKeywords(keyword: String) {
+        dataStore.edit { preferences ->
+            val currentSet =
+                preferences[PreferencesKey.preferKeyWords]?.toMutableSet() ?: mutableSetOf()
+            currentSet.remove(keyword)
+            Log.d("addPreferCategories", currentSet.toString())
+            preferences[PreferencesKey.preferKeyWords] = currentSet
         }
     }
 }
--- app/src/main/java/com/example/androidudemycoupon/util/Constants.kt
+++ app/src/main/java/com/example/androidudemycoupon/util/Constants.kt
@@ -1,12 +1,35 @@
 package com.example.androidudemycoupon.util
 
 class Constants {
-    companion object{
+    companion object {
         const val COUPON_TABLE_NAME = "coupon_table_name"
         const val COUPON_DATABASE_NAME = "coupon_database_name"
         const val BASE_URL_API = "http://huythanh0x.ddns.net:8080"
 
         const val DATA_STORE_NAME = "data_store_name"
         const val PREFERENCES_FETCHED_DATE_TIME = "preferences_fetched_date_time"
+        const val PREFERENCES_DARK_MODE = "preferences_dark_mode"
+        const val PREFERENCES_COUPON_NOTIFICATION = "preferences_coupon_notification"
+        const val PREFERENCES_PREFER_CATEGORIES = "preferences_categories"
+        const val PREFERENCES_PREFER_KEYWORDS = "preferences_keywords"
+        const val PREFERENCES_NOTIFICATION_BY_KEYWORDS = "preferences_notification_by_keywords"
+        const val PREFERENCES_NOTIFICATION_BY_CATEGORIES = "preferences_notification_by_categories"
+        val CATEGORIES = listOf(
+            "Development",
+            "Business",
+            "Finance & Accounting",
+            "IT & Software",
+            "Office Productivity",
+            "Personal Development",
+            "Design",
+            "Marketing",
+            "Lifestyle",
+            "Photography & Video",
+            "Health & Fitness",
+            "Music",
+            "Teaching & Academics",
+            "Language",
+            "Test Preparation"
+        )
     }
 }
